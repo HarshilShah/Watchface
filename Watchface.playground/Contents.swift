71cbf4694fd5577c8a58beddbfd9745467087462
harshilshah <harshilshah1910@outlook.com>
2017-04-02 22:54:55 +0530

    Added a UIPageViewController to display all faces

--- Watchface.playground/Contents.swift
+++ Watchface.playground/Contents.swift
@@ -3,9 +3,43 @@
 import UIKit
 import PlaygroundSupport
 
-let chunky = ChunkyWatchFace()
-chunky.view.frame = CGRect(x: 0, y: 0, width: 312, height: 390)
+class WatchfaceGallery: UIPageViewController, UIPageViewControllerDataSource {
+    
+    private let watchfaces = [
+        ChunkyWatchFace(), AnalogWatchFace(), NumeralsWatchFace(),
+        SevenSegmentWatchFace(), CalculatorWatchFace()
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        setViewControllers([watchfaces.first!], direction: .forward, animated: false, completion: nil)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if var index = watchfaces.index(of: viewController) {
+            index += watchfaces.count - 1
+            index %= watchfaces.count
+            return watchfaces[index]
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if var index = watchfaces.index(of: viewController) {
+            index += 1
+            index %= watchfaces.count
+            return watchfaces[index]
+        }
+        
+        return nil
+    }
+}
 
-PlaygroundPage.current.liveView = chunky.view
+let gallery = WatchfaceGallery(transitionStyle: .scroll, navigationOrientation: .horizontal, options: [:])
 
-chunky.set()
+gallery.view.frame = CGRect(x: 0, y: 0, width: 312, height: 390)
+
+PlaygroundPage.current.liveView = gallery.view
